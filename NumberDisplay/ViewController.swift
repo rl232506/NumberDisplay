2215e36b0a018996b4a956a48505710e6073287b
MICHAEL DUMITRESCU <rl232506@rlas-macair-23.local>
2018-09-24 11:19:56 -0500

    Initial Commit

---
+++ NumberDisplay/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NumberDisplay
+//
+//  Created by MICHAEL DUMITRESCU on 9/24/18.
+//  Copyright © 2018 MICHAEL DUMITRESCU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
